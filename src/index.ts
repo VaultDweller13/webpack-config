6f9187dc4ad02201786ff17d85c56031c605b7c8
VaultDweller13 <grognak13@gmail.com>
2023-01-19 00:00:52 +0400

    feat: add assets processing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import "./index.css";
+// import catImg from "./cat.png";
 
 const a: string = "Hello";
+const img = document.createElement("img");
+img.classList.add("img");
+// img.src = catImg;
+document.body.append(img);
 
 console.log(a);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   resolve: {
